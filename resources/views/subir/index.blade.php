23b72cf13f47eb0f3c1e602799119942f1eb97b1
Daniel Fontalva Ostio <145746911+Danielfo684@users.noreply.github.com>
2024-12-02 17:38:14 +0100

    c

--- resources/views/subir/index.blade.php
+++ resources/views/subir/index.blade.php
@@ -3,17 +3,19 @@
 @section('title', 'Lista de Archivos')
 
 @section('content')
-    <div class="container">
-        <h2>All files</h2>
-        <ul class="gallery">
-            @foreach ($archivos as $archivo)
-                <li>
-                    <a href="{{ route('subir.show', $archivo->id) }}">
-                        <img src="{{ route('imagenes.view', basename($archivo->nombre)) }}" alt="{{ $archivo->nombre_original }}" style="max-width: 400px; height: auto;">
-                        <p>{{ $archivo->nombre_original }}</p>
-                    </a>
-                </li>
-            @endforeach
-        </ul>
-    </div>
+<div class="container">
+    <h2>All files</h2>
+    <ul class="gallery">
+        @foreach ($archivos as $archivo)
+            <li>
+                <a href="{{ route('subir.show', $archivo->id) }}">
+                    <p>{{ $archivo->nombre_original }}</p>
+                    <img src="{{ route('imagenes.view', basename($archivo->nombre)) }}"
+                        alt="{{ $archivo->nombre_original }}" style="max-width: 400px; height: auto;">
+
+                </a>
+            </li>
+        @endforeach
+    </ul>
+</div>
 @endsection
