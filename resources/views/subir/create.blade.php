08194bd9904294ad216f8d2d39a4aa5ccbf85a70
Daniel Fontalva Ostio <145746911+Danielfo684@users.noreply.github.com>
2024-12-02 18:13:02 +0100

    j

--- resources/views/subir/create.blade.php
+++ resources/views/subir/create.blade.php
@@ -4,20 +4,15 @@
 
 @section('content')
 <div class="container">
-    <div class="main-button">
-        <h2>Upload Your File Here</h2>
-    </div>
-
-
     <form action="{{ route('subir.store') }}" method="POST" enctype="multipart/form-data" class="upload-form">
         @csrf
         <input type="file" name="archivo" id="archivo" accept="image/*" required>
 
         <button type="submit"><div class="upload-button">Upload File</div></button>
     </form>
-
     <a href="{{ route('subir.index') }}">
-        <div class="back-button">Go Back</div>
+    <div class="back-button">Go Back</div>
+   
 </div>
 </div>
 @endsection
